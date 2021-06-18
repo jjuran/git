0283a68b7a701bfc59636c526bbab38704897f46
Josh Juran <jjuran@metamage.com>
2021-06-18 17:23:18 -0400

    Exit without global destruction in start_command()
    The environ storage is shared between parent and child of reexec() -- so
    it's critical not to destroy it in the child.  This fixes a crash due to
    memory corruption when using a git alias in MacRelix that only showed up
    after modifying the environ storage to refrain from throwing exceptions.

--- run-command.c
+++ run-command.c
@@ -424,7 +424,7 @@ fail_pipe:
 			if (!cmd->silent_exec_failure)
 				error("cannot run %s: %s", cmd->argv[0],
 					strerror(ENOENT));
-			exit(127);
+			_exit(127);
 		} else {
 			die_errno("cannot exec '%s'", cmd->argv[0]);
 		}
