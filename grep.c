588295e7d0f53fc3c25ae08544c6cf828e253b07
Josh Juran <jjuran@metamage.com>
2023-11-19 05:09:00 -0500

    Explicitly return after calling noreturn functions

--- grep.c
+++ grep.c
@@ -1337,6 +1337,9 @@ int grep_source_load(struct grep_source *gs)
 		return gs->buf ? 0 : -1;
 	}
 	die("BUG: invalid grep_source type");
+
+	/* Not reached */
+	return 0;
 }
 
 void grep_source_load_driver(struct grep_source *gs)
